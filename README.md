a5a6fd7136cd5338d0baaed915cb00d5dd31e279
modass-cppu <72234728+modass-cppu@users.noreply.github.com>
2020-11-14 14:05:26 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DustBin
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x, y, diameter){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2    
+        }
+        this.x = x
+        this.y = y
+        this.diameter = diameter
+
+        this.body = Bodies.circle(this.x, this.y, this.diameter/2, options)
+
+        World.add(world, this.body)
+
+    }
+    display(){
+        var paperpos = this.body.position
+        push()
+        translate(paperpos.x, paperpos.y)
+        ellipseMode(RADIUS)
+        strokeWeight(3)
+        fill(255, 0, 255)
+        ellipse(0, 0, this.diameter, this.diameter)
+        pop()
+    }
+}
